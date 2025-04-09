a4f4c1f8c4665741c51ac6f410552c9126d856f0
EloyR <eloy.roger.araya@gmail.com>
2025-04-09 17:51:15 +0000

    prueba

--- src/domain.js
+++ src/domain.js
@@ -1,21 +1,45 @@
-let pronoun1 = ['the', 'our'];
-let adj1 = ['great', 'big'];
-let noun1 = ['jogger', 'racoon'];
-let exten1 = ['.com','.es']
+const PRONOUM = ['the', 'our'];
+const ADJ = ['great', 'big'];
+const NOUN = ['jogger', 'racoon'];
+const EXTEN = ['.com','.es']
 
-for (let i = 0; i < pronoun1.length; i++) {
-  for (let j = 0; j < adj1.length; j++) 
-    for (let k = 0; k < noun1.length; k++) {
-        for (let l = 0; l < exten1.length; l++) {
-      console.log(pronoun1[i] + adj1[j] + noun1[k] + exten1[l]);
-    }
-  }
+
+const domain = () =>{
+  PRONOUM.forEach(function(p) {
+      ADJ.forEach(function(a) {
+        NOUN.forEach(function(n) {
+          EXTEN.forEach(function(e) {
+            // console.log(p + a + n + e);
+          });
+        });
+      });
+    });
+    return (p + a + n + e)
 }
+
+console.log(domain);
+
+
+
 
 // let pronoun = ['the', 'our'];
 // let adj = ['great', 'big'];
 // let noun = ['jogger', 'racoon'];
 // let exten = ['.com','.es']
+
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < adj.length; j++) 
+//     for (let k = 0; k < noun.length; k++) {
+//         for (let l = 0; l < exten.length; l++) {
+//       console.log(pronoun[i] + adj[j] + noun[k] + exten[l]);
+//     }
+//   }
+// }
+
+
+
+
+
 
 
 // pronoun.forEach(function(p) {
